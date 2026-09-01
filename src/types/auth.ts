2b0b90cf4959066b5ea135507452178db740627b
NiklasSod <nma.prob@gmail.com>
2026-09-02 00:30:38 +0200

    add interface register

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -7,3 +7,10 @@ export interface LoginRequest {
   email: string
   password: string
 }
+
+export interface RegisterRequest {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
