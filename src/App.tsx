fcbf93186fe1282a68fc0113ba300b05efa11860
Anton <antonroback@gmail.com>
2026-09-02 08:34:06 +0200

    feat: basic create course implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import LoginView from './pages/LoginView'
 import CreateAccountView from './pages/CreateAccountView'
+import CreateCourseView from './pages/CreateCourseView'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Route path="/login" element={<LoginView />} />
       <Route path="/register" element={<CreateAccountView />} />
       <Route path="/create-account" element={<CreateAccountView />} />
+      <Route path="/create-course" element={<CreateCourseView />} />
+      <Route path="/courses/create" element={<CreateCourseView />} />
       <Route path="*" element={<p>Page not found.</p>} />
     </Routes>
   )
