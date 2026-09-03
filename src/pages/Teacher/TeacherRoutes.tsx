8e9d722476067105c24e622f133e8f8c65dfc10d
NiklasSod <nma.prob@gmail.com>
2026-09-03 10:37:46 +0200

    teachers to see (get) on modules too

--- src/pages/Teacher/TeacherRoutes.tsx
+++ src/pages/Teacher/TeacherRoutes.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import CurrentModulesView from '../CurrentModulesView'
 import CoursesRoutes from './Courses/CoursesRoutes'
 
 function TeacherRoutes() {
@@ -6,6 +7,7 @@ function TeacherRoutes() {
     <Routes>
       <Route index element={<p className="p-4">Teacher Dashboard</p>} />
       <Route path="courses/*" element={<CoursesRoutes />} />
+      <Route path="modules" element={<CurrentModulesView />} />
       <Route path="*" element={<p className="p-4">Page not found.</p>} />
     </Routes>
   )
