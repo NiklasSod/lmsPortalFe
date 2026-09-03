7ade10a557e7c7279d97ea123144958daafa2676
NiklasSod <nma.prob@gmail.com>
2026-09-03 10:08:52 +0200

    improve teacher routing

---
+++ src/pages/Teacher/Courses/CoursesRoutes.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+import CourseMembersPage from '../../CourseMembersPage'
+import CreateCourseView from './CreateCourseView'
+import TeacherCourses from './TeacherCourses'
+
+function CoursesRoutes() {
+  return (
+    <Routes>
+      <Route index element={<TeacherCourses />} />
+      <Route path="create" element={<CreateCourseView />} />
+      {/* Placeholder for the teacher course detail page */}
+      <Route path=":courseId" />
+      <Route path=":courseId/students" element={<CourseMembersPage />} />
+      <Route path="*" element={<p className="p-4">Page not found.</p>} />
+    </Routes>
+  )
+}
+
+export default CoursesRoutes
--- src/pages/Teacher/TeacherRoutes.tsx
+++ src/pages/Teacher/TeacherRoutes.tsx
@@ -1,20 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-import CourseMembersPage from '../CourseMembersPage'
-import CreateCourseView from './Courses/CreateCourseView'
-import TeacherCourses from './Courses/TeacherCourses'
+import CoursesRoutes from './Courses/CoursesRoutes'
 
 function TeacherRoutes() {
   return (
     <Routes>
       <Route index element={<p className="p-4">Teacher Dashboard</p>} />
-      <Route path="courses" element={<TeacherCourses />} />
-      <Route path="courses/create" element={<CreateCourseView />} />
-      {/* Placeholder for the teacher course detail page */}
-      <Route path="courses/:id" />
-      <Route
-        path="courses/:courseId/students"
-        element={<CourseMembersPage />}
-      />
+      <Route path="courses/*" element={<CoursesRoutes />} />
       <Route path="*" element={<p className="p-4">Page not found.</p>} />
     </Routes>
   )
