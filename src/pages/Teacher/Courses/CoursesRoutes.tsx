2edc01879885a434c8f3acfba2de8e582213161e
NiklasSod <nma.prob@gmail.com>
2026-09-04 12:26:11 +0200

    fix merge conflict err

--- src/pages/Teacher/Courses/CoursesRoutes.tsx
+++ src/pages/Teacher/Courses/CoursesRoutes.tsx
@@ -3,6 +3,7 @@ import CourseMembersPage from '../../CourseMembersPage'
 import CoursesPage from '../../CoursesPage'
 import CreateCourseView from './CreateCourseView'
 import CourseOverviewView from '../../CourseOverviewView'
+import CourseModulesView from '../../CourseModulesView'
 
 function CoursesRoutes() {
   return (
@@ -11,6 +12,7 @@ function CoursesRoutes() {
       <Route path="create" element={<CreateCourseView />} />
       <Route path=":courseId" element={<CourseOverviewView />} />
       <Route path=":courseId/members" element={<CourseMembersPage />} />
+      <Route path=":courseId/modules" element={<CourseModulesView />} />
       <Route path="*" element={<p className="p-4">Page not found.</p>} />
     </Routes>
   )
--- src/pages/Teacher/TeacherRoutes.tsx
+++ src/pages/Teacher/TeacherRoutes.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import ModulesView from '../ModulesView'
 import CoursesRoutes from './Courses/CoursesRoutes'
-import CourseModulesView from '../CourseModulesView'
-import CourseOverviewView from '../CourseOverviewView'
 import UsersPage from './Admin/UsersPage'
 
 function TeacherRoutes() {
@@ -11,8 +9,6 @@ function TeacherRoutes() {
       <Route index element={<p className="p-4">Teacher Dashboard</p>} />
       <Route path="courses/*" element={<CoursesRoutes />} />
       <Route path="modules" element={<ModulesView />} />
-      <Route path="courses/:courseId" element={<CourseOverviewView />} />
-      <Route path="courses/:courseId/modules" element={<CourseModulesView />} />
       <Route path="admin/users" element={<UsersPage />} />
       <Route path="*" element={<p className="p-4">Page not found.</p>} />
     </Routes>
